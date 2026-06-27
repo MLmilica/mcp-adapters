2b71faf58a4bc335c057ab6552397d0061491920
Milica Boskovic <milicaboskovic91@gmail.com>
2026-06-27 19:36:30 +0200

    Add langchain client and update dependencies
    - Introduced langchain_client.py for basic asynchronous interaction with langchain MCP.
    - Enhanced main.py with additional imports and StdioServerParameters for server communication.
    - Updated pyproject.toml to include isort and langchain dependencies.
    - Modified uv.lock to reflect new package versions.

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import asyncio
-import os
 from dotenv import load_dotenv  
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
 
 load_dotenv()
 
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/milicaboskovic/Projects/mcp-adapters/servers/math_server.py"]
+)
+
+
 async def main():
     print("Hello from mcp-adapters!")
-    print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
